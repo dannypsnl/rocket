6388fbd9506d4e1c8068ed928e8e26a96c681c0c
dannypsnl <dannypsnl@gmail.com>
2017-10-09 22:07:33 +0800

    Show url, and found I should handle URL handcraft

--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,7 @@ type Handler struct {
 
 func (rh *Handler) Handle(w http.ResponseWriter, r *http.Request) {
 	// TODO: 取得真實的url以做對應
+	fmt.Fprintf(w, "%q\n", r.URL.Path)
 	res := rh.Do()
 	fmt.Fprintf(w, res)
 }
