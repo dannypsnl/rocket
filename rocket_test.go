2e7feb0b2b3e502b1d641e6a387eb72a5c871e04
dannypsnl <dannypsnl@gmail.com>
2017-10-22 19:23:53 +0800

    Put response & Builder into response.go

--- rocket_test.go
+++ rocket_test.go
@@ -58,32 +58,6 @@ func TestContextType(t *testing.T) {
 	}
 }
 
-type response struct {
-	contentType string
-	messages    []string
-}
-
-type ResponseBuilder struct {
-	contentType string
-	messages    []string
-}
-
-func (rb *ResponseBuilder) Done() response {
-	defer func() {
-		rb.contentType = ""
-		rb.messages = []string{}
-	}()
-	return response{
-		contentType: rb.contentType,
-		messages:    rb.messages,
-	}
-}
-
-func (rb *ResponseBuilder) ContentType(contentType string) *ResponseBuilder {
-	rb.contentType = contentType
-	return rb
-}
-
 func TestResponseBuilder(t *testing.T) {
 	rb := ResponseBuilder{}
 	if rb.Done().contentType != "" {
