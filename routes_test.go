75b30990465e8554a17572c0e9dbe7b8ed430a22
dannypsnl <dannypsnl@gmail.com>
2017-12-07 11:50:37 +0800

    add test case

--- routes_test.go
+++ routes_test.go
@@ -11,4 +11,12 @@ func TestHttpMethodHandlerCreaterAPI(t *testing.T) {
 	if hello.method != "POST" {
 		t.Error(`function[Post] should create a POST handler, but it's`, hello.method, `handler`)
 	}
+	hello = Put("/", func(Context) Response { return "hello" })
+	if hello.method != "PUT" {
+		t.Error(`function[Put] should create a PUT handler, but it's`, hello.method, `handler`)
+	}
+	hello = Delete("/", func(Context) Response { return "hello" })
+	if hello.method != "DELETE" {
+		t.Error(`function[Delete] should create a DELETE handler, but it's`, hello.method, `handler`)
+	}
 }
