8bb22ea85b734bf32b7fe50040d54f811161955b
dannypsnl <dannypsnl@gmail.com>
2017-12-28 10:14:43 +0800

    test matchArray::Swap

--- match_sort_test.go
+++ match_sort_test.go
@@ -13,6 +13,14 @@ func TestMatchArrayLen(t *testing.T) {
 	}
 }
 
+func TestMatchArraySwap(t *testing.T) {
+	ma := matchArray{"/", "/home/index"}
+	expected := matchArray{"/home/index", "/"}
+	if ma.Swap(0, 1); ma[0] != expected[0] || ma[1] != expected[1] {
+		t.Error("error")
+	}
+}
+
 func TestMatchArrayLess(t *testing.T) {
 	ma := matchArray{"/", "/home/index"}
 	for i := 0; i < 100; i++ {
