25c9d05f14724c568b1caefc80a2284c81616122
dannypsnl <dannypsnl@gmail.com>
2018-08-14 21:08:47 +0800

    test: rocket_test can't work on circle-ci

--- example/main.go
+++ example/main.go
@@ -10,8 +10,8 @@ type User struct {
 	Age  int    `route:"age"`
 }
 
-var hello = rk.Get("/:name/age/:age", func(user User) rk.Res {
-	return rk.Res(fmt.Sprintf("Hello %s, your age is %s\n", user.Name, user.Age))
+var hello = rk.Get("/:name/age/:age", func(user *User) rk.Res {
+	return rk.Res(fmt.Sprintf("Hello %s, your age is %d\n", user.Name, user.Age))
 })
 
 func main() {
