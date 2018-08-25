1e71b7b5759acd2b07e3089c8ac7ecc118f61b88
dannypsnl <dannypsnl@gmail.com>
2018-08-25 22:11:46 +0800

    test: remove unneeded test

--- router_test.go
+++ router_test.go
@@ -8,25 +8,24 @@ import (
 )
 
 type User struct {
-	Name    string `route:"name"`
-	Age     uint32 `route:"age"`
-	TestInt int    `route:"test-int"`
+	Name string `route:"name"`
+	Age  uint32 `route:"age"`
 }
 
 func TestRoute(t *testing.T) {
 	assert := assert.NewTester(t)
 
-	route := "/world/:name/:age/:test-int"
+	route := "/world/:name/:age"
 	handler := handlerByMethod(&route, func(u *User) string {
-		return fmt.Sprintf("User{name: `%s`, age: `%d`}, testInt: %d", u.Name, u.Age, u.TestInt)
+		return fmt.Sprintf("User{name: `%s`, age: `%d`}", u.Name, u.Age)
 	}, "GET")
 
 	r := NewRoute()
 	r.addHandlerTo("/hello"+handler.route, handler)
 
 	t.Run("Call", func(t *testing.T) {
-		actual := r.Call("/hello/world/danny/21/100")
+		actual := r.Call("/hello/world/danny/21")
 
-		assert.Eq(actual, "User{name: `danny`, age: `21`}, testInt: 100")
+		assert.Eq(actual, "User{name: `danny`, age: `21`}")
 	})
 }
