b314e251ea85f6f048d2ea03ec9d98ce56c94537
kw <shana0440@gmail.com>
2018-08-28 18:29:03 +0800

    feat: handle patch method (#27)
    * feat: handle patch method
    
    * test: add patch method testcase

--- handler.go
+++ handler.go
@@ -97,6 +97,11 @@ func Put(route string, do interface{}) *handler {
 	return handlerByMethod(&route, do, "PUT")
 }
 
+// Patch return a patch handler.
+func Patch(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "PATCH")
+}
+
 // Delete return delete handler.
 func Delete(route string, do interface{}) *handler {
 	return handlerByMethod(&route, do, "DELETE")
