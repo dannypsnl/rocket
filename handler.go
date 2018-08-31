0abd125105a634947ed905de8b17b6ee98de9990
dannypsnl <dannypsnl@gmail.com>
2018-09-01 00:16:31 +0800

    style: handler creator be indepent to be more clean

--- handler.go
+++ handler.go
@@ -123,28 +123,3 @@ func handlerByMethod(route *string, do interface{}, method string) *handler {
 
 	return h
 }
-
-// Get return a get handler.
-func Get(route string, do interface{}) *handler {
-	return handlerByMethod(&route, do, "GET")
-}
-
-// Post return a post handler.
-func Post(route string, do interface{}) *handler {
-	return handlerByMethod(&route, do, "POST")
-}
-
-// Put return a put handler.
-func Put(route string, do interface{}) *handler {
-	return handlerByMethod(&route, do, "PUT")
-}
-
-// Patch return a patch handler.
-func Patch(route string, do interface{}) *handler {
-	return handlerByMethod(&route, do, "PATCH")
-}
-
-// Delete return delete handler.
-func Delete(route string, do interface{}) *handler {
-	return handlerByMethod(&route, do, "DELETE")
-}
---
+++ handler_creator.go
@@ -0,0 +1,26 @@
+package rocket
+
+// Get return a get handler.
+func Get(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "GET")
+}
+
+// Post return a post handler.
+func Post(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "POST")
+}
+
+// Put return a put handler.
+func Put(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "PUT")
+}
+
+// Patch return a patch handler.
+func Patch(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "PATCH")
+}
+
+// Delete return delete handler.
+func Delete(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "DELETE")
+}
