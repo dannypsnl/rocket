b539e40aaedfd80f2d3a7f0f28ace9e8394bb2d3
dannypsnl <dannypsnl@gmail.com>
2018-09-08 20:34:44 +0800

    test: move spec test run into testCases

--- http_method_test.go
+++ http_method_test.go
@@ -16,19 +16,14 @@ func TestHandlerCreatorHttpMethod(t *testing.T) {
 		{"PUT", Put},
 		{"PATCH", Patch},
 		{"DELETE", Delete},
+		{"SelfDefined", func(route string, do interface{}) *handler {
+			return handlerByMethod(&route, do, "SelfDefined")
+		}},
 	}
 
 	for _, testCase := range testCases {
 		testMethod(t, testCase.method, testCase.handlerCreator)
 	}
-
-	t.Run("SelfDefinedMethod", func(t *testing.T) {
-		route := "/"
-		h := handlerByMethod(&route, func() {}, "Self")
-		if h.method != "Self" {
-			t.Errorf("expected: %v, actual: %v", "Self", h.method)
-		}
-	})
 }
 
 func testMethod(t *testing.T, method string, handlerCreator func(route string, do interface{}) *handler) {
