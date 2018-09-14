d74067f0b4100ebe1597467d49dfdd517e4c15e2
dannypsnl <dannypsnl@gmail.com>
2018-09-15 00:01:04 +0800

    (#63) test: finally remove these shit

--- server_test.go
+++ server_test.go
@@ -1,17 +1,11 @@
 package rocket_test
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"strings"
 	"testing"
 
-	"github.com/dannypsnl/assert"
 	"github.com/dannypsnl/rocket"
+	"github.com/gavv/httpexpect"
 )
 
 type User struct {
@@ -49,7 +43,7 @@ var (
 	forPatch = rocket.Patch("/patch", func(f *ForPatch) string {
 		return f.Val
 	})
-	user = rocket.Get("/:name/name/", func(u *User) string {
+	user = rocket.Get("/name/:name/", func(u *User) string {
 		return u.Name
 	})
 	query = rocket.Get("/query", func(u *User) string {
@@ -68,8 +62,6 @@ var (
 )
 
 func TestServer(t *testing.T) {
-	assert := assert.NewTester(t)
-
 	rk := rocket.Ignite(":8080").
 		Mount("/", homePage, staticFiles).
 		Mount("/users", user).
@@ -80,124 +72,72 @@ func TestServer(t *testing.T) {
 	ts := httptest.NewServer(rk)
 	defer ts.Close()
 
+	e := httpexpect.New(t, ts.URL)
+
 	t.Run("GetUserName", func(t *testing.T) {
-		result, err := response("GET", ts.URL, "/users/Danny/name")
-		assert.Eq(err, nil)
-		assert.Eq(result, "Danny")
+		e.GET("/users/name/Danny").
+			Expect().Status(200).
+			Body().Equal("Danny")
 	})
 
 	t.Run("GetHomePage", func(t *testing.T) {
-		result, err := response("GET", ts.URL, "/")
-		assert.Eq(err, nil)
-		assert.Eq(result, `
+		e.GET("/").
+			Expect().Status(200).
+			Body().Equal(`
 		<h1>Title</h1>
 		<p>Hello, World</p>
 		`)
 	})
 
 	t.Run("MatchPathParameter", func(t *testing.T) {
-		result, err := response("GET", ts.URL, "/static/index.js")
-		assert.Eq(err, nil)
-		assert.Eq(result, `index.js`)
-		result, err = response("GET", ts.URL, "/static/css/index.css")
-		assert.Eq(err, nil)
-		assert.Eq(result, `css/index.css`)
+		e.GET("/static/index.js").
+			Expect().Status(200).
+			Body().Equal(`index.js`)
+		e.GET("/static/css/index.css").
+			Expect().Status(200).
+			Body().Equal(`css/index.css`)
 	})
 
 	t.Run("Post", func(t *testing.T) {
-		var jsonStr = []byte(`{"value":"for post"}`)
-		req, err := http.NewRequest("POST", ts.URL+"/test/post", bytes.NewBuffer(jsonStr))
-		req.Header.Set("Content-Type", "application/json")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(err)
+		jsonObj := map[string]interface{}{
+			"value": "for post",
 		}
-		defer resp.Body.Close()
-
-		var response ForPost
-		err = json.NewDecoder(resp.Body).Decode(&response)
-
-		assert.Eq(err, nil)
-		assert.Eq(resp.Header.Get("Content-Type"), "application/json")
-		assert.Eq(response.Val, "response")
+		expected := map[string]interface{}{
+			"value": "response",
+		}
+		e.POST("/test/post").WithJSON(jsonObj).
+			Expect().Status(200).
+			ContentType("application/json", "").
+			JSON().Equal(expected)
 	})
 
 	t.Run("Patch", func(t *testing.T) {
-		result, err := request("PATCH", ts.URL, "/test/patch", url.Values{
-			"value": {"patch"},
-		})
-		assert.Eq(err, nil)
-		assert.Eq(result, "patch")
+		e.PATCH("/test/patch").WithFormField("value", "patch").
+			Expect().Status(200).
+			Body().Equal("patch")
 	})
 
 	t.Run("Query", func(t *testing.T) {
-		result, err := response("GET", ts.URL, "/test/query?name=Danny")
-		assert.Eq(err, nil)
-		assert.Eq(result, "Danny")
+		e.GET("/test/query").WithQuery("name", "Danny").
+			Expect().Status(200).
+			Body().Equal("Danny")
 	})
 
 	t.Run("Cookies", func(t *testing.T) {
-		result, err := response("GET", ts.URL, "/test/cookies")
-		assert.Eq(err, nil)
-		assert.Eq(result, "cookies")
+		e.GET("/test/cookies").
+			Expect().Status(200).
+			Body().Equal("cookies")
 	})
 
 	t.Run("EndWithSlash", func(t *testing.T) {
-		result, err := response("GET", ts.URL, "/test/end-with-slash")
-		assert.Eq(err, nil)
-		assert.Eq(result, "you found me")
+		e.GET("/test/end-with-slash").
+			Expect().Status(200).
+			Body().Equal("you found me")
 	})
 
 	t.Run("Handle404NotFound", func(t *testing.T) {
-		result, err := response("GET", ts.URL, "/404")
-		assert.Eq(err, nil)
-		assert.Eq(result, "<h1>Page Not Found</h1>")
+		e.GET("/404").
+			Expect().Status(404).
+			Body().Equal("<h1>Page Not Found</h1>")
 	})
-}
-
-func request(method, serverUrl, route string, values url.Values) (string, error) {
-	body := strings.NewReader(values.Encode())
-	req, err := http.NewRequest(method, serverUrl+route, body)
-	if err != nil {
-		return "", err
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	c := &http.Client{}
-	resp, err := c.Do(req)
-	if err != nil {
-		return "", err
-	}
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	err = resp.Body.Close()
-	if err != nil {
-		return "", err
-	}
-	return string(b), nil
-}
-
-func response(method, serverUrl, route string) (string, error) {
-	req, err := http.NewRequest(method, serverUrl+route, bytes.NewBuffer([]byte(``)))
-	if err != nil {
-		return "", err
-	}
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	err = resp.Body.Close()
-	if err != nil {
-		return "", err
-	}
-
-	return string(b), nil
 }
