130910c26fa803450548a9bff95f373a19a040b1
dannypsnl <dannypsnl@gmail.com>
2018-09-18 12:56:58 +0800

    refactor: func `Cookie` ~> `Get`

--- cookies.go
+++ cookies.go
@@ -8,7 +8,7 @@ type Cookies struct {
 	req *http.Request
 }
 
-func (c *Cookies) Cookie(name string) (*http.Cookie, error) {
+func (c *Cookies) Get(name string) (*http.Cookie, error) {
 	return c.req.Cookie(name)
 }
 
