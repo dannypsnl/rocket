a5ebce07d6c1447c49fc67b6e172a06078a22c2e
dannypsnl <dannypsnl@gmail.com>
2018-10-27 03:18:50 +0800

    add: pointer as optional field

--- helper.go
+++ helper.go
@@ -25,83 +25,87 @@ func parseParameter(v reflect.Value, param string) (reflect.Value, error) {
 	case reflect.Bool:
 		r, err := strconv.ParseBool(param)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*bool)(nil)), invaildType
 		}
 		return reflect.ValueOf(r), nil
 	case reflect.Int:
 		r, err := strconv.ParseInt(param, 10, 0)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*int)(nil)), invaildType
 		}
 		return reflect.ValueOf(int(r)), nil
 	case reflect.Int8:
 		r, err := strconv.ParseInt(param, 10, 8)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*int8)(nil)), invaildType
 		}
 		return reflect.ValueOf(int8(r)), nil
 	case reflect.Int16:
 		r, err := strconv.ParseInt(param, 10, 16)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*int16)(nil)), invaildType
 		}
 		return reflect.ValueOf(int16(r)), nil
 	case reflect.Int32:
 		r, err := strconv.ParseInt(param, 10, 32)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*int32)(nil)), invaildType
 		}
 		return reflect.ValueOf(int32(r)), nil
 	case reflect.Int64:
 		r, err := strconv.ParseInt(param, 10, 64)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*int64)(nil)), invaildType
 		}
 		return reflect.ValueOf(int64(r)), nil
 	case reflect.Uint:
 		r, err := strconv.ParseUint(param, 10, 0)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*uint)(nil)), invaildType
 		}
 		return reflect.ValueOf(uint(r)), nil
 	case reflect.Uint8:
 		r, err := strconv.ParseUint(param, 10, 8)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*uint8)(nil)), invaildType
 		}
 		return reflect.ValueOf(uint8(r)), nil
 	case reflect.Uint16:
 		r, err := strconv.ParseUint(param, 10, 16)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*uint16)(nil)), invaildType
 		}
 		return reflect.ValueOf(uint16(r)), nil
 	case reflect.Uint32:
 		r, err := strconv.ParseUint(param, 10, 32)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*uint32)(nil)), invaildType
 		}
 		return reflect.ValueOf(uint32(r)), nil
 	case reflect.Uint64:
 		r, err := strconv.ParseUint(param, 10, 64)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*uint64)(nil)), invaildType
 		}
 		return reflect.ValueOf(r), nil
 	case reflect.Float32:
 		r, err := strconv.ParseFloat(param, 32)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*float32)(nil)), invaildType
 		}
 		return reflect.ValueOf(float32(r)), nil
 	case reflect.Float64:
 		r, err := strconv.ParseFloat(param, 64)
 		if err != nil {
-			return reflect.Value{}, invaildType
+			return reflect.ValueOf((*float64)(nil)), invaildType
 		}
 		return reflect.ValueOf(r), nil
 	case reflect.String:
 		return reflect.ValueOf(param), nil
+	case reflect.Ptr:
+		// ignore error because can always get expected type(ptr to value) & expected value(nil or value)
+		v, _ := parseParameter(v.Elem(), param)
+		return v, nil
 	default:
 		panic("unsupported parameter type")
 	}
