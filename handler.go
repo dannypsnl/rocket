2d7921f2971a909534054848f3a147a58a3d4701
林子篆 <dannypsnl@gmail.com>
2018-10-27 21:19:04 +0800

    fix: JSON can't work with query & route (#107)
    * fix: JSON can't work with query & route
    
    * refactor: make contextType is struct type(current is pointer to defined
    context struct)
    
    * refactor: remove query index detection, it never been executed

--- handler.go
+++ handler.go
@@ -79,19 +79,8 @@ func (h *handler) needHeader() bool {
 func (h *handler) context(rs []string, req *http.Request) []reflect.Value {
 	param := make([]reflect.Value, h.do.Type().NumIn())
 	if h.hasUserDefinedContext() {
-		contextType := h.do.Type().In(h.userDefinedContextOffset)
-		context := reflect.New(contextType.Elem())
-
-		if h.expectJsonRequest {
-			v := context.Interface()
-			err := json.NewDecoder(req.Body).Decode(v)
-			if err != nil {
-				param[h.userDefinedContextOffset] = reflect.ValueOf(errors.New("400"))
-				return param
-			}
-			param[h.userDefinedContextOffset] = reflect.ValueOf(v)
-			return param
-		}
+		contextType := h.do.Type().In(h.userDefinedContextOffset).Elem()
+		context := reflect.New(contextType)
 
 		for idx, route := range h.routes {
 			if isParameter(route) {
@@ -119,6 +108,17 @@ func (h *handler) context(rs []string, req *http.Request) []reflect.Value {
 				value := parseParameter(context.Elem().Field(idx), p)
 				field.Set(value)
 			}
+		}
+
+		if h.expectJsonRequest {
+			v := context.Interface()
+			err := json.NewDecoder(req.Body).Decode(v)
+			if err != nil {
+				param[h.userDefinedContextOffset] = reflect.ValueOf(errors.New("400"))
+				return param
+			}
+			param[h.userDefinedContextOffset] = reflect.ValueOf(v)
+			return param
 		}
 
 		req.ParseForm()
