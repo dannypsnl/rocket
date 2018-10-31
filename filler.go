0d430d99af5e23771a5d6371dcc9d19c241de926
dannypsnl <dannypsnl@gmail.com>
2018-10-31 16:32:25 +0800

    (#105) refactor: by chain of responsibility pattern

---
+++ filler.go
@@ -0,0 +1,116 @@
+package rocket
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"reflect"
+)
+
+type (
+	contextFiller interface {
+		fill(context reflect.Value) error
+	}
+	nextFiller struct {
+		next contextFiller
+	}
+	routeFiller struct {
+		nextFiller
+		h  *handler
+		rs []string
+	}
+	queryFiller struct {
+		nextFiller
+		h     *handler
+		query url.Values
+	}
+	jsonFiller struct {
+		nextFiller
+		h   *handler
+		req *http.Request
+	}
+	formFiller struct {
+		nextFiller
+		h    *handler
+		form url.Values
+	}
+)
+
+func (n *nextFiller) nextFill(ctx reflect.Value) error {
+	if n.next != nil {
+		return n.next.fill(ctx)
+	}
+	return nil
+}
+func (r *routeFiller) fill(ctx reflect.Value) error {
+	for idx, route := range r.h.routes {
+		if isParameter(route) {
+			param := r.rs[len(r.rs)-len(r.h.routes)+idx]
+			index := r.h.routeParams[idx]
+			v := parseParameter(ctx.Elem().Field(index), param)
+			ctx.Elem().Field(index).
+				Set(v)
+		}
+	}
+	if r.h.matchedPathIndex != -1 {
+		i := r.h.routeParams[r.h.matchedPathIndex]
+		ctx.Elem().Field(i).
+			Set(reflect.ValueOf(r.h.matchedPath))
+	}
+	return r.nextFill(ctx)
+}
+func (q *queryFiller) fill(ctx reflect.Value) error {
+	for k, idx := range q.h.queryParams {
+		field := ctx.Elem().Field(idx)
+		if v, ok := q.query[k]; ok {
+			p := v[0]
+			value := parseParameter(field, p)
+			field.Set(value)
+		}
+	}
+	return q.nextFill(ctx)
+}
+func (j *jsonFiller) fill(ctx reflect.Value) error {
+	if j.h.expectJsonRequest {
+		v := ctx.Interface()
+		err := json.NewDecoder(j.req.Body).Decode(v)
+		if err != nil {
+			return err
+		}
+		ctx.Elem().Set(reflect.ValueOf(v).Elem())
+		return nil
+	}
+	return j.nextFill(ctx)
+}
+func (f *formFiller) fill(ctx reflect.Value) error {
+	for k, idx := range f.h.formParams {
+		if v, ok := f.form[k]; ok {
+			field := ctx.Elem().Field(idx)
+			p := v[0]
+			value := parseParameter(field, p)
+			field.Set(value)
+		}
+	}
+	return f.nextFill(ctx)
+}
+
+func newRouteFiller(h *handler, rs []string, next contextFiller) contextFiller {
+	r := &routeFiller{h: h, rs: rs}
+	r.next = next
+	return r
+}
+func newQueryFiller(h *handler, query url.Values, next contextFiller) contextFiller {
+	r := &queryFiller{h: h, query: query}
+	r.next = next
+	return r
+}
+func newJSONFiller(h *handler, req *http.Request, next contextFiller) contextFiller {
+	r := &jsonFiller{h: h, req: req}
+	r.next = next
+	return r
+}
+func newFormFiller(h *handler, form url.Values, next contextFiller) contextFiller {
+	r := &formFiller{h: h, form: form}
+	r.next = next
+	return r
+}
--- handler.go
+++ handler.go
@@ -2,10 +2,8 @@ package rocket
 
 import (
 	"bytes"
-	"encoding/json"
 	"errors"
 	"net/http"
-	"net/url"
 	"reflect"
 
 	"github.com/dannypsnl/rocket/response"
@@ -78,74 +76,25 @@ func (h *handler) needHeader() bool {
 	return h.headerOffset != -1
 }
 
-func (h *handler) fillRoute(context reflect.Value, rs []string) {
-	for idx, route := range h.routes {
-		if isParameter(route) {
-			param := rs[len(rs)-len(h.routes)+idx]
-			index := h.routeParams[idx]
-			value := parseParameter(context.Elem().Field(index), param)
-			context.Elem().Field(index).
-				Set(value)
-		}
-	}
-	if h.matchedPathIndex != -1 {
-		index := h.routeParams[h.matchedPathIndex]
-		context.Elem().Field(index).
-			Set(reflect.ValueOf(h.matchedPath))
-	}
-}
-func (h *handler) fillQuery(context reflect.Value, querys url.Values) {
-	for k, idx := range h.queryParams {
-		field := context.Elem().Field(idx)
-		if v, ok := querys[k]; ok {
-			p := v[0]
-			value := parseParameter(context.Elem().Field(idx), p)
-			field.Set(value)
-		}
-	}
-}
-func (h *handler) fillJSON(context reflect.Value, req *http.Request) (reflect.Value, error) {
-	if h.expectJsonRequest {
-		v := context.Interface()
-		err := json.NewDecoder(req.Body).Decode(v)
-		if err != nil {
-			return reflect.Value{}, err
-		}
-		return reflect.ValueOf(v), nil
-	}
-	return context, nil
-}
-func (h *handler) fillForm(context reflect.Value, form url.Values) {
-	for k, idx := range h.formParams {
-		if v, ok := form[k]; ok {
-			p := v[0]
-			value := parseParameter(context.Elem().Field(idx), p)
-			context.Elem().Field(idx).
-				Set(value)
-		}
-	}
-}
-
-type contextParser interface {
-	parse(context reflect.Value, rs []string, vs url.Values)
-}
-
 func (h *handler) context(rs []string, req *http.Request) []reflect.Value {
 	param := make([]reflect.Value, h.do.Type().NumIn())
 	if h.hasUserDefinedContext() {
 		contextType := h.do.Type().In(h.userDefinedContextOffset).Elem()
 		context := reflect.New(contextType)
 
-		h.fillRoute(context, rs)
-		h.fillQuery(context, req.URL.Query())
-		v, err := h.fillJSON(context, req)
+		req.ParseForm()
+		filler := newRouteFiller(h, rs,
+			newQueryFiller(h, req.URL.Query(),
+				newJSONFiller(h, req,
+					newFormFiller(h, req.Form, nil),
+				),
+			),
+		)
+		err := filler.fill(context)
 		if err != nil {
 			param[h.userDefinedContextOffset] = reflect.ValueOf(errors.New("400"))
 			return param
 		}
-		param[h.userDefinedContextOffset] = v
-		req.ParseForm()
-		h.fillForm(context, req.Form)
 
 		param[h.userDefinedContextOffset] = context
 	}
