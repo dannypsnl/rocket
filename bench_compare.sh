fe9b4f59264d379101a5e7e65317a9dc6531a6b8
dannypsnl <dannypsnl@gmail.com>
2018-12-23 17:20:14 +0800

    fix: can't checkout master

--- bench_compare.sh
+++ bench_compare.sh
@@ -1,4 +1,9 @@
 if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ]; then
+	REMOTE_URL="$(git config --get remote.origin.url)";
+	cd ${TRAVIS_BUILD_DIR}/.. && \
+	git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench" && \
+	cd "${TRAVIS_REPO_SLUG}-bench" && \
+	git checkout ${TRAVIS_COMMIT} && \
 	go test -bench . ./... > current_bench && \
 	git checkout master && \
 	go test -bench . ./... > master_bench && \
