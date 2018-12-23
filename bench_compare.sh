96fe3db244aef6af372b5c8a392af45f99e75cbe
dannypsnl <dannypsnl@gmail.com>
2018-12-23 17:24:33 +0800

    fix: try to fix detach HEAD problem

--- bench_compare.sh
+++ bench_compare.sh
@@ -3,10 +3,10 @@ if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ]; then
 	cd ${TRAVIS_BUILD_DIR}/.. && \
 	git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench" && \
 	cd "${TRAVIS_REPO_SLUG}-bench" && \
-	git checkout ${TRAVIS_COMMIT} && \
-	go test -bench . ./... > current_bench && \
 	git checkout master && \
-	go test -bench . ./... > master_bench && \
+	go test -bench . ./... > master_bench.out && \
+	git checkout ${TRAVIS_COMMIT} && \
+	go test -bench . ./... > current_bench.out && \
 	go get golang.org/x/tools/cmd/benchcmp && \
-	benchcmp master_bench current_bench;
+	benchcmp master_bench.out current_bench.out;
 fi
