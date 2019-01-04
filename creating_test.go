d5e9e10b3a49f0c7f3110537dd773a3ba47e64ea
dannypsnl <dannypsnl@gmail.com>
2019-01-05 03:16:51 +0800

    test: duplicate route is creating test

--- creating_test.go
+++ creating_test.go
@@ -46,3 +46,31 @@ func testMethod(t *testing.T, method string, handlerCreator func(route string, d
 		assert.Eq(method, h.method)
 	})
 }
+
+var (
+	hello = Get("/hello/*", func() string { return "" })
+)
+
+func TestDuplicatedRoute(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Must panic when route emit duplicated!")
+		}
+	}()
+	Ignite(":8080").
+		Mount("/", hello, hello)
+}
+
+func TestDuplicateRoutePanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r != PanicDuplicateRoute {
+			t.Error("panic message is wrong or didn't panic")
+		}
+	}()
+	var (
+		root1 = Get("/", func() string { return "" })
+		root2 = Get("/", func() string { return "" })
+	)
+	Ignite(":80888").
+		Mount("/", root1, root2)
+}
