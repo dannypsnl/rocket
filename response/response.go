917a841b52ca17e320d8d383092e4a4aca46e68e
dannypsnl <dannypsnl@gmail.com>
2019-01-11 15:47:13 +0800

    (#99) fix: change status code twice or with invalid code would panic now

--- response/response.go
+++ response/response.go
@@ -27,7 +27,81 @@ func New(body interface{}) *Response {
 	}
 }
 
+var validStatusCode = map[int]bool{
+	http.StatusContinue:           true,
+	http.StatusSwitchingProtocols: true,
+	http.StatusProcessing:         true,
+
+	http.StatusOK:                   true,
+	http.StatusCreated:              true,
+	http.StatusAccepted:             true,
+	http.StatusNonAuthoritativeInfo: true,
+	http.StatusNoContent:            true,
+	http.StatusResetContent:         true,
+	http.StatusPartialContent:       true,
+	http.StatusMultiStatus:          true,
+	http.StatusAlreadyReported:      true,
+	http.StatusIMUsed:               true,
+
+	http.StatusMultipleChoices:  true,
+	http.StatusMovedPermanently: true,
+	http.StatusFound:            true,
+	http.StatusSeeOther:         true,
+	http.StatusNotModified:      true,
+	http.StatusUseProxy:         true,
+
+	http.StatusTemporaryRedirect: true,
+	http.StatusPermanentRedirect: true,
+
+	http.StatusBadRequest:                   true,
+	http.StatusUnauthorized:                 true,
+	http.StatusPaymentRequired:              true,
+	http.StatusForbidden:                    true,
+	http.StatusNotFound:                     true,
+	http.StatusMethodNotAllowed:             true,
+	http.StatusNotAcceptable:                true,
+	http.StatusProxyAuthRequired:            true,
+	http.StatusRequestTimeout:               true,
+	http.StatusConflict:                     true,
+	http.StatusGone:                         true,
+	http.StatusLengthRequired:               true,
+	http.StatusPreconditionFailed:           true,
+	http.StatusRequestEntityTooLarge:        true,
+	http.StatusRequestURITooLong:            true,
+	http.StatusUnsupportedMediaType:         true,
+	http.StatusRequestedRangeNotSatisfiable: true,
+	http.StatusExpectationFailed:            true,
+	http.StatusTeapot:                       true,
+	http.StatusMisdirectedRequest:           true,
+	http.StatusUnprocessableEntity:          true,
+	http.StatusLocked:                       true,
+	http.StatusFailedDependency:             true,
+	http.StatusUpgradeRequired:              true,
+	http.StatusPreconditionRequired:         true,
+	http.StatusTooManyRequests:              true,
+	http.StatusRequestHeaderFieldsTooLarge:  true,
+	http.StatusUnavailableForLegalReasons:   true,
+
+	http.StatusInternalServerError:           true,
+	http.StatusNotImplemented:                true,
+	http.StatusBadGateway:                    true,
+	http.StatusServiceUnavailable:            true,
+	http.StatusGatewayTimeout:                true,
+	http.StatusHTTPVersionNotSupported:       true,
+	http.StatusVariantAlsoNegotiates:         true,
+	http.StatusInsufficientStorage:           true,
+	http.StatusLoopDetected:                  true,
+	http.StatusNotExtended:                   true,
+	http.StatusNetworkAuthenticationRequired: true,
+}
+
 func (res *Response) Status(code int) *Response {
+	if !validStatusCode[code] {
+		panic(fmt.Errorf("reject invalid status code: %d", code))
+	}
+	if res.statusCode != 0 {
+		panic("status code already be changed, you can't modify it twice!")
+	}
 	res.statusCode = code
 	return res
 }
