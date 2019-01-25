8faf134e99e6b7fa1586f6756e64170eb8d7ccf0
dannypsnl <dannypsnl@gmail.com>
2019-01-25 20:38:08 +0800

    (#116) test: forward action from guard

--- guard_test.go
+++ guard_test.go
@@ -20,20 +20,56 @@ func (h *headerGuard) VerifyRequest(r *http.Request) (rocket.Action, error) {
 	return rocket.Failure, errors.New("not allowed")
 }
 
+type forwardGuard struct{}
+
+func (f *forwardGuard) VerifyRequest(r *http.Request) (rocket.Action, error) {
+	return rocket.Forward, nil
+}
+
 func TestGuard(t *testing.T) {
-	rk := rocket.Ignite(":8081").
-		Mount("/", rocket.Get("/", func(h *headerGuard) string {
-			return "pass"
-		}))
-	ts := httptest.NewServer(rk)
-	defer ts.Close()
+	testCases := []struct {
+		name           string
+		handlerFunc    interface{}
+		testFunc       func(*httpexpect.Request)
+		expectedStatus int
+	}{
+		{
+			name:        "valid request would pass guard",
+			handlerFunc: func(*headerGuard) string { return "" },
+			testFunc: func(r *httpexpect.Request) {
+				r.WithHeader("Auth", "user1").
+					Expect().
+					Status(http.StatusOK)
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:           "invalid request won't pass guard",
+			handlerFunc:    func(*headerGuard) string { return "" },
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "action forward is give up decision right",
+			handlerFunc:    func(*forwardGuard) string { return "" },
+			expectedStatus: http.StatusOK,
+		},
+	}
 
-	e := httpexpect.New(t, ts.URL)
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			rk := rocket.Ignite("").
+				Mount("/", rocket.Get("/", testCase.handlerFunc))
+			ts := httptest.NewServer(rk)
+			defer ts.Close()
+			e := httpexpect.New(t, ts.URL)
 
-	e.GET("/").
-		Expect().
-		Status(http.StatusBadRequest)
-	e.GET("/").WithHeader("Auth", "user1").
-		Expect().
-		Status(http.StatusOK)
+			request := e.GET("/")
+			if testCase.testFunc != nil {
+				testCase.testFunc(request)
+			} else {
+				request.Expect().
+					Status(testCase.expectedStatus)
+			}
+		})
+	}
 }
