0b47f35ce4e2a1cb5cbc6fd9070b5620b6bfadb8
dannypsnl <dannypsnl@gmail.com>
2019-02-02 15:08:06 +0800

    (#147) fix: matching bug

--- routing_test.go
+++ routing_test.go
@@ -2,6 +2,7 @@ package rocket_test
 
 import (
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/dannypsnl/rocket"
@@ -16,47 +17,57 @@ func TestRouting(t *testing.T) {
 		requestRouteMapToMatchedRoute map[string]string
 	}{
 		{
-			name:   "root",
-			routes: []string{"/"},
+			name:   "no route",
+			routes: make([]string, 0),
 			requestRouteMapToMatchedRoute: map[string]string{
-				"/": "/",
+				"GET;/":  "page not found",
+				"GET;/a": "page not found",
+			},
+		},
+		{
+			name:   "root",
+			routes: []string{"GET;/"},
+			requestRouteMapToMatchedRoute: map[string]string{
+				"GET;/": "/",
 			},
 		},
 		{
 			name: "multi spec route",
 			routes: []string{
-				"/a",
-				"/b",
-				"/a/b",
+				"GET;/a",
+				"GET;/b",
+				"GET;/a/b",
 			},
 			requestRouteMapToMatchedRoute: map[string]string{
-				"/a":   "/a",
-				"/b":   "/b",
-				"/a/b": "/a/b",
+				"GET;/a":   "/a",
+				"GET;/b":   "/b",
+				"GET;/a/b": "/a/b",
 			},
 		},
 		{
 			name: "variant vs spec",
 			routes: []string{
-				"/a",
-				"/:name",
+				"GET;/a",
+				"GET;/:name",
 			},
 			requestRouteMapToMatchedRoute: map[string]string{
-				"/a": "/a",
-				"/b": "/:name",
-				"/c": "/:name",
+				"GET;/a": "/a",
+				"GET;/b": "/:name",
+				"GET;/c": "/:name",
 			},
 		},
 		{
 			name: "wildcard vs spec",
 			routes: []string{
-				"/a",
-				"/*wildcard",
+				"GET;/a",
+				"GET;/*wildcard",
 			},
 			requestRouteMapToMatchedRoute: map[string]string{
-				"/a":   "/a",
-				"/b":   "/*wildcard",
-				"/b/c": "/*wildcard",
+				"GET;/a":      "/a",
+				"GET;/b":      "/*wildcard",
+				"GET;/b/c":    "/*wildcard",
+				"GET;/a/b/c":  "/*wildcard",
+				"POST;/a/b/c": "request resource does not support http method 'POST'",
 			},
 		},
 	}
@@ -65,18 +76,35 @@ func TestRouting(t *testing.T) {
 		t.Run(testCase.name, func(t *testing.T) {
 			rk := rocket.Ignite("")
 			for _, route := range testCase.routes {
-				rk.Mount("/", rocket.Get(route,
-					func(response string) func() string {
-						return func() string { return response }
-					}(route)))
+				handler := rocket.Get("/", func() string { return "wrong" })
+				method, route := splitRuleToMethodRoute(route)
+				handleFunc := func(response string) func() string {
+					return func() string { return response }
+				}(route)
+
+				switch method {
+				case "GET":
+					handler = rocket.Get(route, handleFunc)
+				case "POST":
+					handler = rocket.Post(route, handleFunc)
+				}
+				rk.Mount("/", handler)
 			}
 			ts := httptest.NewServer(rk)
 			defer ts.Close()
 
 			e := httpexpect.New(t, ts.URL)
 			for requestRoute, matchedRoute := range testCase.requestRouteMapToMatchedRoute {
-				e.GET(requestRoute).Expect().Body().Equal(matchedRoute)
+				method, route := splitRuleToMethodRoute(requestRoute)
+				e.Request(method, route).Expect().Body().Equal(matchedRoute)
 			}
 		})
 	}
+}
+
+func splitRuleToMethodRoute(rule string) (method string, route string) {
+	ss := strings.Split(rule, ";")
+	method = ss[0]
+	route = ss[1]
+	return
 }
