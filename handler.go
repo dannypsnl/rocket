a958f84282a3cd61c1cb2b3845f52a9db6d04520
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-03-22 13:06:55 +0800

    merge handler_creator to handler

--- handler.go
+++ handler.go
@@ -10,6 +10,50 @@ import (
 	"github.com/dannypsnl/rocket/router"
 )
 
+// Get return a get handler.
+func Get(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "GET")
+}
+
+// Post return a post handler.
+func Post(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "POST")
+}
+
+// Put return a put handler.
+func Put(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "PUT")
+}
+
+// Patch return a patch handler.
+func Patch(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "PATCH")
+}
+
+// Delete return delete handler.
+func Delete(route string, do interface{}) *handler {
+	return handlerByMethod(&route, do, "DELETE")
+}
+
+func handlerByMethod(route *string, do interface{}, method string) *handler {
+	handlerDo := reflect.ValueOf(do)
+	h := newHandler(handlerDo)
+	h.method = method
+
+	h.route = *route
+	h.routes = router.SplitBySlash(*route)
+
+	handlerFuncT := reflect.TypeOf(do)
+	h.userContexts = make([]*context.UserContext, handlerFuncT.NumIn())
+
+	for i := 0; i < handlerFuncT.NumIn(); i++ {
+		contextT := handlerFuncT.In(i).Elem()
+		h.userContexts[i] = context.NewUserContext().
+			CacheParamsOffset(contextT, h.routes)
+	}
+	return h
+}
+
 type handler struct {
 	route  string
 	routes []string
